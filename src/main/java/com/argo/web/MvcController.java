7202bb1f37682bbb4c27fb7f3fd1d674b91c29c7
Yaming <yamingd@qq.com>
2015-09-10 15:06:46 +0800

    getIP

--- src/main/java/com/argo/web/MvcController.java
+++ src/main/java/com/argo/web/MvcController.java
@@ -101,6 +101,29 @@ public abstract class MvcController {
 
     }
 
+    /**
+     * 获取用户ip地址（在apache代理后需要从头文件中获取ip）
+     *
+     * @param request
+     * @return
+     */
+    public static String getIP(HttpServletRequest request) {
+        String ip = request.getHeader("x-forwarded-for");
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("X-Real-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        return ip;
+    }
+
     public String getMenu(){
         return DEFAULT_MENU;
     }
